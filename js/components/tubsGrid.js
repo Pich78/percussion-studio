bc7f1219f0c170a965424e54df6457c981638e8c
apicciano <apiccianoit@yahoo.it>
2026-01-12 14:14:05 +0100

    Removed "Load from PC button from Mobile App"

--- js/components/tubsGrid.js
+++ js/components/tubsGrid.js
@@ -572,6 +572,7 @@ export const TubsGrid = ({
                         OK
                     </button>
                   ` : `
+                    ${!isMobile ? `
                     <input 
                         type="file" 
                         id="rhythm-file-input" 
@@ -586,6 +587,7 @@ export const TubsGrid = ({
                         ${ComputerDesktopIcon('w-4 h-4 pointer-events-none')}
                         Load from PC
                     </button>
+                    ` : ''}
                     <button 
                         data-action="close-modal"
                         class="px-4 py-2 text-gray-400 hover:text-white font-medium"
